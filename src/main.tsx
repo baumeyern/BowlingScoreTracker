17ef518440fbbdb5e943c54d6f15ff84f67f57ad
baumeyern <baumeyern@gmail.com>
2026-01-30 02:10:55 -0500

    Fix duplicate BrowserRouter error by removing duplicate providers from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'sonner';
 import { BowlerProvider } from './contexts/BowlerContext';
 import App from './App.tsx';
 import './index.css';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60, // 1 minute
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BowlerProvider>
-        <BrowserRouter>
-          <App />
-          <Toaster position="top-center" richColors />
-        </BrowserRouter>
-      </BowlerProvider>
-    </QueryClientProvider>
+    <BowlerProvider>
+      <App />
+    </BowlerProvider>
   </StrictMode>
 );
